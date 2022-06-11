ec6e1c759cb1373a8b81ad2cbab4b87204cbea01
kary <1301604100@qq.com>
2022-06-11 15:19:09 +0800

    混淆矩阵、准确度acc、精确度p、召回率r

--- pred.py
+++ pred.py
@@ -1,14 +1,18 @@
 import tensorflow as tf
 import numpy as np
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from cnn_train import load_data
 import matplotlib.pyplot as plt
 
+plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
 
-def show_heatmaps(title, x_labels, y_labels, harvest, save_name):
-    # 这里是创建一个画布
+
+def show_heatmaps(x_labels, y_labels, cm, save_name):
+    # 创建一个画布
     fig, ax = plt.subplots()
 
-    im = ax.imshow(harvest, cmap="Blues")
+    im = ax.imshow(cm, cmap="Blues")
 
     ax.set_xticks(np.arange(len(y_labels)))
     ax.set_yticks(np.arange(len(x_labels)))
@@ -16,19 +20,19 @@ def show_heatmaps(title, x_labels, y_labels, harvest, save_name):
     # 添加每个热力块的具体数值
     for i in range(len(x_labels)):
         for j in range(len(y_labels)):
-            text = ax.text(j, i, round(harvest[i, j], 2),
-                           ha="center", va="center", color="black")
-    ax.set_xlabel("Predict label")
-    ax.set_ylabel("Actual label")
-    ax.set_title(title)
+            ax.text(j, i, round(cm[i, j], 2), ha="center", va="center", color="black")
+    # ax.set_xlabel("Predict label")
+    # ax.set_ylabel("Actual label")
+
+    plt.rc("font", family='Microsoft YaHei')
+
+    ax.set_title("混淆矩阵")
     fig.tight_layout()
     plt.colorbar(im)
     plt.savefig(save_name, dpi=100)
-    # plt.show()
 
 
 def predict(model_name):
-
     train_dataset, test_dataset, class_names = load_data()
 
     # 加载模型
@@ -39,43 +43,68 @@ def predict(model_name):
     loss, accuracy = model.evaluate(test_dataset)
 
     # 输出结果
-    print('test accuracy :', accuracy)
+    print('测试准确度 :', accuracy)
 
     test_real_labels = []  # 真实结果类名
-    test_pre_labels = []   # 测试结果类名
-    for test_batch_images, test_batch_labels in test_dataset:
-        test_batch_labels = test_batch_labels.numpy()
-        test_batch_pres = model.predict(test_batch_images)
-        # print(test_batch_pres)
+    test_pre_labels = []  # 测试结果类名
+    for test_images, test_labels in test_dataset:
+        test_labels = test_labels.numpy()  # 取出类名数组  0  1
+        test_pres = model.predict(test_images)  # 进行预测
 
-        test_batch_labels_max = np.argmax(test_batch_labels, axis=1)
-        test_batch_pres_max = np.argmax(test_batch_pres, axis=1)
-        # print(test_batch_labels_max)
-        # print(test_batch_pres_max)
-        # 将推理对应的标签取出
-        for i in test_batch_labels_max:
+        test_labels_max = np.argmax(test_labels, axis=1)  # 取出最大值所对应的索引
+        test_pres_max = np.argmax(test_pres, axis=1)  # 取出最大值所对应的索引
+
+        # 取出真实结果的类名
+        for i in test_labels_max:
             test_real_labels.append(i)
-
-        for i in test_batch_pres_max:
+        # 取出预测结果的类名
+        for i in test_pres_max:
             test_pre_labels.append(i)
-        # break
 
-    # print(test_real_labels)
-    # print(test_pre_labels)
+    # 类名数量
     class_names_length = len(class_names)
-    heat_maps = np.zeros((class_names_length, class_names_length))
-    for test_real_label, test_pre_label in zip(test_real_labels, test_pre_labels):
-        heat_maps[test_real_label][test_pre_label] = heat_maps[test_real_label][test_pre_label] + 1
 
-    print(heat_maps)
-    heat_maps_sum = np.sum(heat_maps, axis=1).reshape(-1, 1)
-    # print(heat_maps_sum)
+    # 初始化混淆矩阵
+    cm = np.zeros((class_names_length, class_names_length))
+    # 构建混淆矩阵
+    for test_real_label, test_pre_label in zip(test_real_labels, test_pre_labels):
+        cm[test_real_label][test_pre_label] += 1
+
+    print(cm)
+
+    # 每一行的总数
+    cm_sum = np.sum(cm, axis=1).reshape(-1, 1)
+
+    # 测试集数量
+    n = len(test_real_labels)
+
+    # https://blog.csdn.net/qwe1110/article/details/103391632
+    t = p = r = 0
+    for i in range(class_names_length):
+        temp_p1 = temp_p2 = temp_r = 0
+        for j in range(class_names_length):
+            if i == j:
+                t += cm[i][j]
+                temp_p2 = cm[i][j]
+            temp_p1 += cm[j][i]
+            temp_r += cm[i][j]
+        p += (temp_p2 / temp_p1)
+        r += (temp_p2 / temp_r)
+    print("准确度acc:", t / n)
+    print("精确度p:", p / class_names_length)
+    print("召回率r:", r / class_names_length)
+    # print("f1:", 2 * p * r / (p + r))
+
     print()
-    heat_maps_float = heat_maps / heat_maps_sum
-    print(heat_maps_float)
-    # title, x_labels, y_labels, harvest
-    show_heatmaps(title="heatmap", x_labels=class_names, y_labels=class_names, harvest=heat_maps_float,
-                  save_name="results/heatmap_"+ model_name +".png")
+    # 获得概率的混淆矩阵
+    cm_float = cm / cm_sum
+    print(cm_float)
+
+    # 热力图
+    show_heatmaps(x_labels=class_names,
+                  y_labels=class_names,
+                  cm=cm_float,
+                  save_name="results/heatmap_" + model_name + ".png")
 
 
 if __name__ == '__main__':
